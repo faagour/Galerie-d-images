56b9e4bdc620bcfba6005b4631ee0a5f8abe5f47
faagour <144700964+faagour@users.noreply.github.com>
2024-02-10 21:09:26 +0100

    hello

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
+var thumbnails = document.querySelectorAll('.thumbnail');
+var currentIndex = 0; // Index de l'image actuellement affichée
 
+// Fonction pour changer l'image principale
+function changeMainImage(newIndex) {
+    var mainImage = document.getElementById('main-image');
+    var selectedThumbnail = thumbnails[newIndex];
+    mainImage.src = selectedThumbnail.src;
+    currentIndex = newIndex;
+}
+
+// Clic sur la flèche gauche
+document.getElementById('left-arrow').addEventListener('click', function() {
+    if(currentIndex > 0) {
+        changeMainImage(currentIndex - 1);
+    }
+});
+
+// Clic sur la flèche droite
+document.getElementById('right-arrow').addEventListener('click', function() {
+    if(currentIndex < thumbnails.length - 1) {
+        changeMainImage(currentIndex + 1);
+    }
+});
+
+// Mise à jour de l'image principale au clic sur une vignette
+for (var i = 0; i < thumbnails.length; i++) {
+    (function(index){
+        thumbnails[index].onclick = function() {
+            changeMainImage(index);
+        };
+    })(i);
+}
